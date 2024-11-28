58b80ab7caac7c85669162412e413ee624e1161f
Fernando Gustavo <fernando.gdev@gmail.com>
2024-11-27 21:12:15 -0300

    adicionando url

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-
-// https://fullheros.onrender.com
-
 export default axios.create({
-    baseURL: `http://localhost:3000/`
+    baseURL: `https://fullheros.onrender.com`
 })
